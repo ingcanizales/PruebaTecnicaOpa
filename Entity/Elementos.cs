b1cbcd2717ff4a65ee0848b2f6d40a7fbc413870
jimmy <jimmy.asprilla@microshif.com.co>
2023-10-28 20:17:35 -0500

    Creación de clase y de listas

---
+++ Entity/Elementos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaTecnicaOpa.Entity
+{
+    public class Elementos
+    {
+        public int Id { get; set; }
+        public double Peso { get; set; }
+        public double Caloria { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using PruebaTecnicaOpa.Entity;
+using System.Collections.Generic;
+using System.Linq;
 namespace PruebaTecnicaOpa
 {
     class Program
@@ -14,5 +16,22 @@ namespace PruebaTecnicaOpa
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
+    }
+
+    public class ControlElementos
+    {
+        public ControlElementos()
+        {
+            _listaClientes = new List<Elementos>();
+
+
+            _listaClientes.Add(new Elementos { Id = 1, Peso = 5, Caloria = 3 });
+            _listaClientes.Add(new Elementos { Id = 2, Peso = 3, Caloria = 5 });
+            _listaClientes.Add(new Elementos { Id = 3, Peso = 5, Caloria = 2 });
+            _listaClientes.Add(new Elementos { Id = 1, Peso = 1, Caloria = 8 });
+            _listaClientes.Add(new Elementos { Id = 2, Peso = 2, Caloria = 3 });
+        }
+
+        public List<Elementos> _listaClientes;
     }
 }
