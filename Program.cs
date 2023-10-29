797ffbe1ad8511a2f0545beb27c4fa749a74382c
jimmy <jimmy.asprilla@microshif.com.co>
2023-10-29 16:47:54 -0500

    Ajuste a la consulta

--- Program.cs
+++ Program.cs
@@ -25,43 +25,89 @@ namespace PruebaTecnicaOpa
             _listaElementos.Add(new Elementos { Id = 1, Codigo = "E1" ,Peso = 5, Caloria = 3 });
             _listaElementos.Add(new Elementos { Id = 2, Codigo = "E2", Peso = 3, Caloria = 5 });
             _listaElementos.Add(new Elementos { Id = 3, Codigo = "E3", Peso = 5, Caloria = 2 });
-            _listaElementos.Add(new Elementos { Id = 1, Codigo = "E5", Peso = 1, Caloria = 8 });
-            _listaElementos.Add(new Elementos { Id = 2, Codigo = "E6", Peso = 2, Caloria = 3 });
+            _listaElementos.Add(new Elementos { Id = 4, Codigo = "E4", Peso = 1, Caloria = 8 });
+            _listaElementos.Add(new Elementos { Id = 5, Codigo = "E5", Peso = 2, Caloria = 3 });
         }
 
         public List<Elementos> _listaElementos;
+        public List<Elementos> _listaElementosSumados = new List<Elementos>();
 
         public void query()
         {
             //se realiza el ingeso y la lectura de los datos.
             string pesoIngresado;
             string caloriaIngresada;
+            var sumCaloria = 0.0;
+            var sumPeso = 0.0;
+
             Console.WriteLine("Señor usuario ingrese el peso máximo");
             pesoIngresado = Console.ReadLine();
 
-            Console.WriteLine("Señor usuario ingrese la cantidad máxima de caloría");
+            Console.WriteLine("Señor usuario ingrese la cantidad mínima de caloría");
             caloriaIngresada = Console.ReadLine();
             double peso = int.Parse(pesoIngresado);
             double caloria = int.Parse(caloriaIngresada);
 
             //Se realiza la consulta utilizando linq.
-            var consulta = _listaElementos.Where(elemento => elemento.Peso < peso && elemento.Caloria < caloria);
-            if (consulta != null)
+            var consulta = _listaElementos.ToList();
+            foreach (var item in consulta)
             {
-                //Se muestran los elementos filtrados.
-                Console.WriteLine("Los elementos seleccionados tienen las siguientes caracteristicas: ");
-                foreach (var item in consulta)
+                var minCal = sumCaloria + item.Caloria;
+                var pesMax = sumPeso + item.Peso;
+
+                if (minCal > caloria && pesMax < peso)
                 {
-                    Console.Write($" {item.Codigo} con peso: {item.Peso} y calorías: {item.Caloria} {Environment.NewLine}");
+                    _listaElementosSumados.Add(item);
+                    sumCaloria = _listaElementosSumados.Select(s => s.Caloria).Sum();
+                    sumPeso = _listaElementosSumados.Select(s => s.Peso).Sum();
+                }
+
+
+                if (minCal <= caloria && pesMax < peso)
+                {
+                    _listaElementosSumados.Add(item);
+
+                    sumCaloria = _listaElementosSumados.Select(s => s.Caloria).Sum();
+                    sumPeso = _listaElementosSumados.Select(s => s.Peso).Sum();
+                }
+
+
+                if ( sumPeso >= peso) break;
+            }
+
+
+            if (_listaElementos != null)
+            {
+
+                //Se realiza la suma de los valores
+
+                if (sumCaloria < caloria)
+                {
+                    Console.WriteLine(" No existen elementos con las caracteristicas solicitadas.");
+                }
+                else
+                {
+                    if (sumPeso > 0 && sumPeso < peso && sumCaloria > caloria)
+                    {
+                        Console.WriteLine("Los elementos seleccionados tienen las siguientes caracteristicas: ");
+                        foreach (var item in _listaElementosSumados)
+                        {
+                            Console.Write($" {item.Codigo} con peso: {item.Peso} y calorías: {item.Caloria} {Environment.NewLine}");
+
+
+                        }
+                    }
+                    Console.WriteLine($" los valores totales de calorías son: {sumCaloria} y de peso peso: {sumPeso}");
 
                 }
+
             }
-            
-                
+
+
         }
 
     }
-    
 
-    
+
+
 }
