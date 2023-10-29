1739a6909679787b57fdff185bf9632b75d070d8
jimmy <jimmy.asprilla@microshif.com.co>
2023-10-28 20:52:59 -0500

    Creación de Consulta

--- Program.cs
+++ Program.cs
@@ -8,13 +8,8 @@ namespace PruebaTecnicaOpa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            ///
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            ControlElementos control = new ControlElementos();
+            control.query();          
         }
     }
 
@@ -22,16 +17,41 @@ namespace PruebaTecnicaOpa
     {
         public ControlElementos()
         {
-            _listaClientes = new List<Elementos>();
+            _listaElementos = new List<Elementos>();
 
 
-            _listaClientes.Add(new Elementos { Id = 1, Peso = 5, Caloria = 3 });
-            _listaClientes.Add(new Elementos { Id = 2, Peso = 3, Caloria = 5 });
-            _listaClientes.Add(new Elementos { Id = 3, Peso = 5, Caloria = 2 });
-            _listaClientes.Add(new Elementos { Id = 1, Peso = 1, Caloria = 8 });
-            _listaClientes.Add(new Elementos { Id = 2, Peso = 2, Caloria = 3 });
+            _listaElementos.Add(new Elementos { Id = 1, Peso = 5, Caloria = 3 });
+            _listaElementos.Add(new Elementos { Id = 2, Peso = 3, Caloria = 5 });
+            _listaElementos.Add(new Elementos { Id = 3, Peso = 5, Caloria = 2 });
+            _listaElementos.Add(new Elementos { Id = 1, Peso = 1, Caloria = 8 });
+            _listaElementos.Add(new Elementos { Id = 2, Peso = 2, Caloria = 3 });
         }
 
-        public List<Elementos> _listaClientes;
+        public List<Elementos> _listaElementos;
+
+        public void query()
+        {
+            string pesoIngresado;
+            string caloriaIngresada;
+            Console.WriteLine("Señor usuario ingrese el peso");
+            pesoIngresado = Console.ReadLine();
+
+            Console.WriteLine("Señor usuario ingrese la cantidad de caloría");
+            caloriaIngresada = Console.ReadLine();
+            double peso = int.Parse(pesoIngresado);
+            double caloria = int.Parse(caloriaIngresada);
+
+            var consulta = _listaElementos.Where(elemento => elemento.Peso < peso && elemento.Caloria < caloria);
+            foreach (var item in consulta)
+            {
+                Console.Write($"los Elementos recomendados son peso {item.Peso} y calorías {item.Caloria} {Environment.NewLine}");
+
+            }
+                
+        }
+
     }
+    
+
+    
 }
